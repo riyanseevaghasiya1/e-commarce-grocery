0ceebffbbb9a24ba9f8967ef1d501f8c86c2cef3
HstiPrajapati17 <hastiprajapati15@gmail.com>
2026-01-07 09:10:26 +0530

    dfghjkl;

--- js/home.js
+++ js/home.js
@@ -72,8 +72,8 @@ $(document).ready(function () {
             1024: { items: 3 },
             480: { items: 2 },
             768: { items: 3 },
-            992: { items: 4 },
-            1200: { items: 4 }
+            992: { items: 3 },
+            1200: { items: 3 }
         }
     });
 
